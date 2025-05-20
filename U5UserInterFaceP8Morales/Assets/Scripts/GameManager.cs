d588b63a518bc91869c708bc6479210fcaf33364
DavidPatino8 <719048@student.dusd.net>
2025-05-20 15:25:44 -0700

    Progress
    Made more progress. Check Unity tutorial for where to start.

--- U5UserInterFaceP8Morales/Assets/Scripts/GameManager.cs
+++ U5UserInterFaceP8Morales/Assets/Scripts/GameManager.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    private int score;
     private float spawnRate = 1.0f;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        // Spawns eight ball, skull, etc. 
         StartCoroutine(SpawnTarget());
+        //To start score
+        score = 0;
+        UpdateScore(0);
+
+        gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -29,5 +39,11 @@ public class GameManager : MonoBehaviour
             Instantiate(targets[index]);
         }
         
+    }
+    //updates score
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
