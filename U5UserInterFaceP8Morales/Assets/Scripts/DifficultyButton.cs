1e41253999bf24c22824cc13c2fd98216c3c3959
TheWaspsAreHere <719048@student.dusd.net>
2025-05-30 17:48:04 -0700

    Completed
    Completed the Unit 5. Need to complete Challenge now.

--- U5UserInterFaceP8Morales/Assets/Scripts/DifficultyButton.cs
+++ U5UserInterFaceP8Morales/Assets/Scripts/DifficultyButton.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DifficultyButton : MonoBehaviour
 {
     private Button button;
     //Need to reference game manager script
     private GameManager gameManager;
 
+    public int difficulty;
+
     // Start is called before the first frame update
     void Start()
     {
         button = GetComponent<Button>();
-        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
-        //program will listen for when button is clicked
-        button.onClick.AddListener(SetDifficulty);
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        // program will listen for when button is clicked
+        button.onClick.AddListener(SetDifficulty); 
     }
 
     // Update is called once per frame
@@ -27,6 +29,6 @@ public class NewBehaviourScript : MonoBehaviour
     void SetDifficulty()
     {
         Debug.Log(button.gameObject.name + " was clicked");
-        gameManager.StartGame();
+        gameManager.StartGame(difficulty);
     }
 }
--- U5UserInterFaceP8Morales/Assets/Scripts/GameManager.cs
+++ U5UserInterFaceP8Morales/Assets/Scripts/GameManager.cs
@@ -10,9 +10,14 @@ public class GameManager : MonoBehaviour
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI livesText;
     public Button restartButton;
+    public GameObject titleScreen;
+    public GameObject pauseScreen;
+    private bool paused;
     public bool isGameActive;
     private int score;
+    private int lives;
     private float spawnRate = 1.0f;
 
 
@@ -26,7 +31,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //check if user has press P key
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
     }
 
     IEnumerator SpawnTarget()
@@ -45,6 +54,16 @@ public class GameManager : MonoBehaviour
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
     }
+
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if(lives <= 0)
+        {
+            GameOver();
+        }
+    }
     public void GameOver()
     {
         restartButton.gameObject.SetActive(true);
@@ -57,13 +76,33 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    public void StartGame()
+    public void StartGame(int difficulty)
     {
         isGameActive = true;
         score = 0;
+        spawnRate /= difficulty;
         // Spawns eight ball, skull, etc. 
         StartCoroutine(SpawnTarget());
         //To start score
         UpdateScore(0);
+        UpdateLives(3);
+
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1; 
+        }
     }
 }
